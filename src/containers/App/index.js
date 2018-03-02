652e696a3bb5050ffca51ee72567e56e80d42b08
hazratgs <hazratgs@gmail.com>
2018-03-02 03:03:10 +0300

    Тестовое задание Membrana

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,12 +2,18 @@ import React, { PureComponent } from 'react'
 import { withRouter } from 'react-router-dom'
 import s from './style.pcss'
 
+import Header from '../../components/Header'
+import Table from '../Table'
+
 @withRouter
 export default class App extends PureComponent {
   render () {
     return (
       <div className={s.app} onClick={this.handle}>
-        <div className={s.content}></div>
+        <div className={s.content}>
+          <Header/>
+          <Table/>
+        </div>
       </div>
     )
   }
--- src/reducers/App.js
+++ src/reducers/App.js
@@ -1,7 +1,52 @@
-const initialState = {}
+import {
+  GET_LOAD_DATA,
+  SUCCESS_LOAD_DATA,
+  ERROR_LOAD_DATA,
+  CHANGE_SELECT_TOKEN,
+  CLEAR_DATA
+} from '../constants/App'
 
-export default function headerState (state = initialState, action) {
+const initialState = {
+  data: [],
+  startLoadData: false,
+  errorLoadData: false,
+  tokens: [
+    {
+      name: 'EOS',
+      type: 'Blockchain',
+      token: '0x86Fa049857E0209aa7D9e616F7eb3b3B78ECfdb0'
+    },
+    {
+      name: 'TRX',
+      type: 'Blockchain',
+      token: '0xf230b790E05390FC8295F4d3F60332c93BEd42e2'
+    },
+    {
+      name: 'VEN',
+      type: 'Blockchain',
+      token: '0xD850942eF8811f2A866692A623011bDE52a462C1'
+    }
+  ],
+  activeToken: 'EOS'
+}
+
+export default function appState (state = initialState, action) {
   switch (action.type) {
+    case GET_LOAD_DATA:
+      return { ...state, startLoadData: action.payload }
+
+    case SUCCESS_LOAD_DATA:
+      return { ...state, data: [ ...state.data, ...action.payload ] }
+
+    case ERROR_LOAD_DATA:
+      return { ...state, errorLoadData: action.payload }
+
+    case CHANGE_SELECT_TOKEN:
+      return { ...state, activeToken: action.payload }
+
+    case CLEAR_DATA:
+      return { ...state, data: action.payload }
+
     default:
       return state
   }
